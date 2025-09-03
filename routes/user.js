8cb2b30b70aaa8f255f09999a3a7d70c9b974b0d
kriya-8620 <krishnakoley9748@gmail.com>
2025-09-03 22:13:54 +0530

    Resolved merge conflict in users routes

--- routes/user.js
+++ routes/user.js
@@ -6,41 +6,32 @@ const passport = require('passport');
 const { saveRedirectUrl } = require("../middleware");
 
 const usersController=require("../controllers/users.js");
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 4c7d39e3bbc23ed1d322c25e129c105b7c2abefd
 router.get("/signup", wrapAsync(usersController.showSignup));
 
 
 
 router.post("/signup", wrapAsync(usersController.submitSignup));
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 4c7d39e3bbc23ed1d322c25e129c105b7c2abefd
 router.get("/login", wrapAsync(usersController.login));
 
 router.post("/login", 
     saveRedirectUrl,
     passport.authenticate("local", {
         failureRedirect: "/login",
-<<<<<<< HEAD
+
         failureFlash: "Invalid username or password",
     }),wrapAsync(usersController.submitLogin));
 
 
-=======
-        failureFlash: true
-    }), wrapAsync(usersController.submitLogin));
->>>>>>> 4c7d39e3bbc23ed1d322c25e129c105b7c2abefd
+
+  
+
 
 // Route for logging out
 router.get('/logout', wrapAsync(usersController.logout));
 
-<<<<<<< HEAD
+
 
 router.get("/send-login-otp", wrapAsync(usersController.sendLoginOTP));
 router.post("/verify-login-otp",wrapAsync(usersController.verifyLoginOTP)  
@@ -60,6 +51,4 @@ router.get("/forgot-password", (req, res) => {
   res.render("users/forgot.ejs");
 });
 
-=======
->>>>>>> 4c7d39e3bbc23ed1d322c25e129c105b7c2abefd
 module.exports = router;
